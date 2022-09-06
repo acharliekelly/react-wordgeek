754de21f663548b5cf6b77c7d27ac885771373ff
Charlie Kelly <charliekelly31415@gmail.com>
2022-09-06 16:18:17 -0400

    individual letter tile

--- src/components/LetterTile.jsx
+++ src/components/LetterTile.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
 import './LetterTile.css';
 
-const LetterTile = ({ letter }) => {
+const LetterTile = ({ letter, isClicked, index, clickHandler }) => {
+
+  const handleClick = () => {
+    clickHandler(index);
+  }
+
   return (
-    <div className="tile">
-      {letter}
-    </div>
+    <Badge md={3} sm={4} xs={6}  
+      className={isClicked ? "clicked" : "init"}
+      bg={isClicked ? "secondary" : "success"}
+      text="light"
+      onClick={handleClick}>{letter}</Badge>
   )
 }
 
