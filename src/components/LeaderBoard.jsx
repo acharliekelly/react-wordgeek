dfef51c576ef0afd2bd91e877c0dfc7972ee8ab6
Charlie Kelly <charliekelly31415@gmail.com>
2022-09-06 16:17:32 -0400

    temporarily unused

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Container, Table } from 'react-bootstrap';
 import './LeaderBoard.css';
 
-const LeaderBoard = ({ topScores }) => {
+const LeaderBoard = ({ scores }) => {
 
   return (
     <Container className="leaderBoard">
+      
+      <h2>Leader Board</h2>
       <Table striped bordered hover>
         <thead>
           <th>#</th>
@@ -14,8 +16,8 @@ const LeaderBoard = ({ topScores }) => {
           <th>Score</th>
         </thead>
         <tbody>
-          {topScores.map((entry, index) => (
-            <tr key={entry.timestamp}>
+          {scores.map((entry, index) => (
+            <tr key={'t' + entry.timestamp}>
               <td>{index}</td>
               <td>{entry.player}</td>
               <td>{entry.word}</td>
