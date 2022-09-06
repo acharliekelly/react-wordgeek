b731fb0292b9ea693def5d7fbe37525c57c01b1e
Charlie Kelly <charliekelly31415@gmail.com>
2022-09-06 16:18:36 -0400

    array of tiles

--- src/components/TileBoard.jsx
+++ src/components/TileBoard.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 import LetterTile from './LetterTile';
 
-const TileBoard = ({ seedString }) => {
-  const letterArr = seedString.split('');
-  
+import './TileBoard.css';
+
+const TileBoard = ({ seedArray, tileClick }) => {
+
+  const arraySize = seedArray.length;
+
   return (
-    <Container style={{'padding':'3em'}}>
-      { letterArr.map((char, idx) => (
-        <LetterTile key={char + idx} letter={char} />
+    <Container className="board">
+      { seedArray.map((tile, idx) => (
+        <LetterTile 
+          key={tile.letter + idx}
+          boardSize={arraySize}
+          letter={tile.letter} isClicked={tile.clicked} index={idx}
+          clickHandler={tileClick} />
       ))}
     </Container>
   )
