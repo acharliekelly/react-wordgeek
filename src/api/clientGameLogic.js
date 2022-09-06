456e05b6e86eb3f837446c36565943334b7a7a3f
Charlie Kelly <charliekelly31415@gmail.com>
2022-09-06 16:15:15 -0400

    update client-side logic

--- src/api/clientGameLogic.js
+++ src/api/clientGameLogic.js
@@ -1,8 +1,6 @@
-import * as topScores from './topscores.json';
+const DEFAULT_STARTING_WORD = "areallylongword";
 
-const DEFAULT_STARTING_WORD = "areallylongwords";
-
-let localWordFrequencyMap = {};
+let localWordFrequencyMap;
 
 /**
  * must come from API to ensure consistency
@@ -12,13 +10,84 @@ export const getStartingWord = () => {
   return DEFAULT_STARTING_WORD;
 }
 
+/**
+ * get one letter tile
+ * @param {string} char 
+ * @returns Tile { letter: string, clicked: boolean }
+ */
+export const letterTile = (char) => {
+  return {
+    letter: char,
+    clicked: false
+  }
+}
+
+/**
+ * get array of tiles from seed word
+ * @param {string} startingWord 
+ * @returns Array<Tile>
+ */
+export const initTilesArray = startingWord => {
+  const arr = startingWord.split('').map(char => letterTile(char));
+  return arr;
+}
+
+/**
+ * 
+ * @returns Map<Character, Integer> of seed word
+ */
 const getInitialFrequencyMap = () => {
-  if (!localWordFrequencyMap)
-    localWordFrequencyMap = characterFrequencyMap(getStartingWord());
+  if (!localWordFrequencyMap || !Object.keys(localWordFrequencyMap).length) {
+    
+    const seedWord = getStartingWord();
+    localWordFrequencyMap = characterFrequencyMap(seedWord);
+  }
+  
   return localWordFrequencyMap;
 }
 
+export const updateFrequencyMap = charMap => {
+  localWordFrequencyMap = charMap;
+}
 
+export const updateStartingWord = seedWord => {
+  localWordFrequencyMap = characterFrequencyMap(seedWord);
+}
+
+/**
+ * randomize an arry
+ * @param {*} array 
+ * @returns randomized array
+ */
+export const shuffleArray = array => {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+export const removeCharFromWord = (word, index) => {
+  const before = word.substring(0, index-1);
+  const after = word.substring(index+1);
+  return before + after;
+}
+
+/**
+ * 
+ * @param {string} testWord 
+ * @returns true if word is valid
+ */
 export const validateWord = testWord => {
   const startingWord = getStartingWord();
   if (testWord.length > startingWord.length)
@@ -29,7 +98,7 @@ export const validateWord = testWord => {
 
 const characterFrequencyMap = word => {
   const hash = {};
-  word.split().forEach(ch => {
+  word.split('').forEach(ch => {
     if (hash[ch])
       hash[ch]++;
     else
@@ -38,23 +107,29 @@ const characterFrequencyMap = word => {
   return hash;
 }
 
+// const charMapToString = charMap => {
+//   let msg = '{ ';
+//   for (const [key, value] of Object.entries(charMap)) {
+//     msg += `${key}: ${value}, `
+//   }
+//   msg += ' }';
+//   return msg;
+// }
+
 const isAnagram = (testWord) => {
 
-  const master = { ...getInitialFrequencyMap() };
+  const master = getInitialFrequencyMap();
+  // console.log(`master set: ` + charMapToString(master));
   const testMap = characterFrequencyMap(testWord);
-
-  return Object.keys(testMap).every(ch => {
-    return (master[ch]) && (master[ch] >= testMap[ch])
-  })
-
-}
-
-
-export const getTopScores = maxResults => {
-  let entries = topScores;
-  if (entries.length > maxResults) {
-    return entries.slice(0, maxResults - 1);
+  // console.log(`test set ("${testWord}"): ` + charMapToString(testMap));
+  let anagram = true;
+  for (const [key, value] of Object.entries(testMap)) {
+    if (!master[key] || master[key] < value) {
+      // console.log(`NOT VALID: master has ${master[key]} instances of ${key} but test has ${value}`)
+      anagram = false;
+      break;
+    }
   }
-  return entries;
-
+  // console.log('VALID')
+  return anagram;
 }
