f4898cdc5f3cb0cb052d6424b1422b61880aa755
Charlie Kelly <charliekelly31415@gmail.com>
2022-09-06 22:12:39 -0400

    move central control to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import GameBoard from './components/GameBoard';
-// import LeaderBoard from './components/LeaderBoard';
+import PlayerPanel from './components/PlayerPanel';
+import LeaderBoard from './components/LeaderBoard';
 import { getStartingWord } from './api/clientGameLogic';
+import { submitWord } from './api/scoreBoard';
+
+import './App.css';
 
 function App() {
-  // const topScores = getTopScores();
+  const [ currentPlayer, setCurrentPlayer ] = useState('');
+  const [ scoresList, setScoresList ] = useState([]);
+
   const startingWord = getStartingWord();
+
+  const updateScores = (submittedWord) => {
+    const tmp = submitWord(currentPlayer, submittedWord, scoresList);
+    if (tmp) {
+      setScoresList(tmp);
+      return true;
+    }
+    return false;
+  }
+
+  const reset = () => {
+    setCurrentPlayer('');
+    setScoresList([]);
+  }
+
+
   return (
     <div className="App">
       <header>
         <h1><strong>WordGeek</strong>{' '}Word Game</h1>
       </header>
-
-      <GameBoard startingWord={startingWord} /> 
-
-      {/* <LeaderBoard topScores={topScores} /> */}
+      <Container>
+        <Row>
+          <Col md={8} sm={12}>
+            <PlayerPanel setPlayerName={setCurrentPlayer} />
+            <GameBoard 
+              startingWord={startingWord} 
+              playerName={currentPlayer} 
+              submitWord={updateScores} 
+              resetFn={reset} /> 
+          </Col>
+          <Col md={4} sm={12}>
+            <LeaderBoard scores={scoresList} />
+          </Col>
+        </Row>
+      </Container>
 
     </div>
   );
